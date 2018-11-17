f5ca162aa45c9d19ef072b865d7ef4c3bc29ec86
Marvin Vrdoljak <marvin@vrdoljak.de>
2018-11-17 19:47:44 +0100

    icons

--- gulpfile.js
+++ gulpfile.js
@@ -8,29 +8,6 @@ var gulp = require("gulp"),
     uglify = require("gulp-uglify"),
     importCss = require('gulp-import-css');
 
-
-var themifyOptions = {
-    palette: {
-        light: {
-            'primary-100': '#f2f2f4',
-            'primary-200': '#cccece',
-            'accent-100': '#e6f9fc',
-            'accent-200': '#96e1ed'
-        },
-        dark: {
-            'primary-100': '#505050',
-            'primary-200': '#666a6b',
-            'accent-100': '#096796',
-            'accent-200': '#0a87c6'
-        }
-    },
-    screwIE11: false,
-    fallback: {
-        cssPath: './dist/theme_fallback.css',
-        dynamicPath: './dist/theme_fallback.json'
-    }
-};
-
 var paths = {
     styles: {
         src: "./src/assets/scss/**/*.scss",
@@ -43,6 +20,10 @@ var paths = {
     images: {
         src: "./src/assets/images/**/*",
         dest: "./dist/images"
+    },
+    fonts: {
+        src: "./src/assets/fonts/**/*",
+        dest: "./dist/fonts"
     }
 };
 
@@ -102,6 +83,11 @@ function images() {
         .pipe(gulp.dest(paths.images.dest));
 }
 
+function fonts() {
+    return gulp.src(paths.fonts.src)
+        .pipe(gulp.dest(paths.fonts.dest));
+}
+
 
 function watch() {
     gulp.watch(paths.styles.src, style)
@@ -114,4 +100,4 @@ function watchjs() {
 
 gulp.task("default", gulp.parallel(watch, watchjs));
 gulp.task("watch", gulp.parallel(watch));
-gulp.task("build", gulp.parallel(styleMinified, scriptsMinified, images));
+gulp.task("build", gulp.parallel(styleMinified, scriptsMinified, images, fonts));
