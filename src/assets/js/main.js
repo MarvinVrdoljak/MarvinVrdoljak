d29bfa81a9723054ac8100aa164a185abeceb908
Marvin Vrdoljak <marvin@vrdoljak.de>
2018-11-18 14:47:07 +0100

    toggle animation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+!function ($) {
+    let $accordionTitle = $('.accordion__title-wrapper');
+
+    $accordionTitle.on('click', function(){
+        $(this).toggleClass('is-active');
+        $(this).next('.accordion__content-wrapper').stop().slideToggle();
+    });
+
+}(jQuery);
